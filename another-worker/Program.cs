6aa9fa4ad2cfd9e0350732df5cf7d0ac5d004b82
Ssemi <ssemibiz@yahoo.co.kr>
2022-01-11 20:56:43 +0900

    init another worker

---
+++ another-worker/Program.cs
@@ -0,0 +1,17 @@
+﻿using another_worker;
+using common_module;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Hello, World!");
+
+var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddTransient<IWorker, anotherWorker>();
+                services.AddTransient<Runner>();
+            })
+            .Build();
+
+var svc = ActivatorUtilities.CreateInstance<Runner>(host.Services);
+await svc.DoAction();
---
+++ another-worker/anotherWorker.cs
@@ -0,0 +1,13 @@
+﻿using common_module;
+
+namespace another_worker;
+
+public class anotherWorker : IWorker
+{
+    public Task RunAsync(string message)
+    {
+        Console.WriteLine(message + $"{nameof(anotherWorker)}");
+
+        return Task.CompletedTask;
+    }
+}
